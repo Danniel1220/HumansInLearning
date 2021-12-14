14c8772cc9bc4234acada7a838e8bfe0f41f88f0
george.clim <climdanniel@gmail.com>
2021-12-14 10:32:49 +0200

    Created a copy of Back-End/Java-Core/Homework-4/OnlineMediaStore-part4.

---
+++ Back-End/Java-Core/Homework-4/OnlineMediaStore-part5/src/com/crystal/dao/DigitalVideoDisc.java
@@ -0,0 +1,24 @@
+package com.crystal.dao;
+
+public class DigitalVideoDisc extends Media {
+    private final String director;
+    private final double length;
+
+    public DigitalVideoDisc(String title, String category, double cost, String director, double length) {
+        super(title, category, cost);
+        this.director = director;
+        this.length = length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String toString() {
+        return super.toString() + "\nDirector: " + this.director + "\nLength: " + this.length + "\n";
+    }
+}
