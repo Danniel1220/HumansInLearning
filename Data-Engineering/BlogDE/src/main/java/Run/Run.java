01007a1e8a706b9edee4f200e6910720410804bf
george.clim <climdanniel@gmail.com>
2021-12-31 12:57:10 +0200

    Data Engineering/BlogDE
    Updated PopulateDB script to write the datetime in milliseconds in 'dateTimeStamp' in the database, and store the provided JSON date in 'date' in database.

--- Data-Engineering/BlogDE/src/main/java/Run/Run.java
+++ Data-Engineering/BlogDE/src/main/java/Run/Run.java
@@ -2,6 +2,10 @@ package Run;
 
 import IO.CLI;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.*;
 import java.sql.*;
 
 public class Run {
@@ -13,7 +17,15 @@ public class Run {
         checkDatabase();
 
         // Start the CLI.
-        CLI CLI = new CLI();
+        //CLI CLI = new CLI();
+
+//        try {
+//            byteArray();
+//        } catch (IOException e) {
+//            System.out.println("rip");
+//            e.printStackTrace();
+//        }
+        testByteArray();
     }
 
     public static void checkDatabase() {
@@ -31,5 +43,51 @@ public class Run {
             e.printStackTrace();
         }
         System.out.println();
+    }
+
+    public static void byteArray() throws IOException {
+        File image = new File("src/main/resources/bucegi.jpg");
+        FileInputStream fileInputStream = new FileInputStream (image);
+
+
+        String sql = "INSERT INTO `blogdb`.`articles` (`title`, `tag`, `author`, `dateTimeStamp`, `imgUrl`, `content`, `imgByte`) VALUES (" +
+                "'title', " +
+                "'tag', " +
+                "'author', " +
+                "'date', " +
+                "'imgurl', " +
+                "'content', " +
+                "'" + fileInputStream + "');";
+
+        System.out.println(fileInputStream);
+
+        // Connecting to database
+        try {
+            Connection connection = DriverManager.getConnection(DATABASE_CONNECT_URL, DATABASE_USER, DATABASE_PASSWORD);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testByteArray() {
+        // Connecting to database
+        try {
+            Connection connection = DriverManager.getConnection(DATABASE_CONNECT_URL, DATABASE_USER, DATABASE_PASSWORD);
+            Statement statement = connection.createStatement();
+            String sql = "select * from articles where id=22";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            //InputStream fileInputStreamString = resultSet.getBinaryStream("byteArray");
+            System.out.println(resultSet.getBinaryStream("imgByte"));
+            RenderedImage image = ImageIO.read(resultSet.getBinaryStream("imgByte"));
+            ImageIO.write(image, "JPG",new File("C:\\Users\\george.clim\\Desktop\\test.jpg"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Data-Engineering/BlogDE/src/main/java/Script/PopulateDB.java
+++ Data-Engineering/BlogDE/src/main/java/Script/PopulateDB.java
@@ -4,14 +4,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PopulateDB {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, java.text.ParseException {
         final String DATABASE_JSON_FILEPATH = "src/main/resources/db.json";
         final String DATABASE_CONNECT_URL = "jdbc:mysql://localhost:3306/blogdb";
         final String DATABASE_USER = "root";
@@ -51,24 +52,26 @@ public class PopulateDB {
             String title = (String) arrObject.get("title");
             String tag = (String) arrObject.get("tag");
             String author = (String) arrObject.get("author");
-            String date = (String) arrObject.get("date");
+            String jsonDate = (String) arrObject.get("date");
+            long millisDate = parseDate(jsonDate);
             String imgUrl = (String) arrObject.get("imgUrl");
             String content = (String) arrObject.get("content");
 
-//            String sql = "INSERT INTO `blogdb`.`articles` (`id`, `title`, `tag`, `author`, `dateTimeStamp`, `imgUrl`, `content`) VALUES (" +
-//                    "'" + id + "', " +
+//            String sql = "INSERT INTO `blogdb`.`articles` (`title`, `tag`, `author`, `date`, `dateTimeStamp`, `imgUrl`, `content`) VALUES (" +
 //                    "'" + title + "', " +
 //                    "'" + tag + "', " +
 //                    "'" + author + "', " +
 //                    "'" + date + "', " +
+//                    "'" + date + "', " +
 //                    "'" + imgUrl + "', " +
 //                    "'" + content + "');";
 
-            String sql = "INSERT INTO `blogdb`.`articles` (`title`, `tag`, `author`, `dateTimeStamp`, `imgUrl`, `content`) VALUES (" +
+            String sql = "INSERT INTO `blogdb`.`articles` (`title`, `tag`, `author`, `date`, `dateTimeStamp`, `imgUrl`, `content`) VALUES (" +
                     "'" + title + "', " +
                     "'" + tag + "', " +
                     "'" + author + "', " +
-                    "'" + date + "', " +
+                    "'" + jsonDate + "', " +
+                    "'" + millisDate + "', " +
                     "'" + imgUrl + "', " +
                     "'" + content + "');";
 
@@ -77,5 +80,67 @@ public class PopulateDB {
         }
 
 
+    }
+
+    public static long parseDate(String inputDate) throws java.text.ParseException {
+        String[] dateParts = inputDate.split(" ");
+        int year = Integer.parseInt(dateParts[2]);
+        int month;
+        int day = Integer.parseInt(dateParts[1].substring(0, dateParts[1].length() - 1));
+
+        switch (dateParts[0]) {
+            case "January":
+                month = 1;
+                break;
+            case "February":
+                month = 2;
+                break;
+            case "March":
+                month = 3;
+                break;
+            case "April":
+                month = 4;
+                break;
+            case "May":
+                month = 5;
+                break;
+            case "June":
+                month = 6;
+                break;
+            case "July":
+                month = 7;
+                break;
+            case "August":
+                month = 8;
+                break;
+            case "September":
+                month = 9;
+                break;
+            case "October":
+                month = 10;
+                break;
+            case "November":
+                month = 11;
+                break;
+            case "December":
+                month = 12;
+                break;
+            default:
+                System.out.println("Month name is invalid, defaulting to January...");
+                month = 1;
+        }
+
+        System.out.println(year);
+        System.out.println(month);
+        System.out.println(day);
+
+        String dateString = year + "/" + month + "/" + day + " 00:00:00";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = simpleDateFormat.parse(dateString);
+
+        System.out.println(date.getTime());
+        System.out.println();
+
+        return date.getTime();
     }
 }
