1d0a8c8705c4b254dba6fa4109ff840e819ddade
george.clim <climdanniel@gmail.com>
2022-01-06 18:46:49 +0200

    Back-End/Java-Core/Polynomial
    Commenting on Polynomial's methods behaviour.

--- Back-End/Java-Core/Polynomial/src/com/crystal/polynomial/Polynomial.java
+++ Back-End/Java-Core/Polynomial/src/com/crystal/polynomial/Polynomial.java
@@ -8,12 +8,17 @@ public class Polynomial {
     }
 
     public void add(Polynomial p) {
+        // If the current array is bigger or equal in size, simply add all the coefficients
+        // from argument's array.
         if (coefficients.length >= p.getCoefficients().length) {
             for (int i = 0; i < coefficients.length; i++)
             {
                 coefficients[i] = coefficients[i] + p.getCoefficients()[i];
             }
         }
+        // If the argument's array is bigger, the field array is too small, so we create a new
+        // one equal in size to the argument's array, compute the new coefficients and assign
+        // the new array's value to the object's field.
         else {
             double[] newCoefficients = p.getCoefficients();
             for (int i = 0; i < coefficients.length; i++)
@@ -35,17 +40,25 @@ public class Polynomial {
     }
 
     public int getDegree() {
+        // Simply iterating through the polynomial array and returning the first non 0 value
+        // will return the degree.
         for (int i = coefficients.length - 1; i >= 0; i--) {
             if (coefficients[i] != 0) return i;
         }
+        // If we found no non 0 values that means the array is full of zeros and the degree is also 0.
         return 0;
     }
 
     public Polynomial getFirstDerivative () {
+        // Create a new array for the first derivative. Array has one less coefficient.
         double[] firstDerivative = new double[coefficients.length - 1];
-        for(int i = coefficients.length - 2; i >= 0; i--) {
+
+        // Iterate through array and compute each coefficient.
+        for(int i = firstDerivative.length - 1; i >= 0; i--) {
             firstDerivative[i] = coefficients[i + 1] * (i + 1);
         }
+
+        // Return a new polynomial object containing the first derivative.
         return new Polynomial(firstDerivative);
     }
 
@@ -53,7 +66,7 @@ public class Polynomial {
     public String toString() {
         String polynomial = "";
         for (int i = coefficients.length - 1; i > 0; i--) {
-            // If it's the first coefficient printed and it's a negative value
+            // If it's the first coefficient printed, and it's a negative value
             if (polynomial.equals("")) {
                 if (coefficients[i] > 0) {
                     polynomial = polynomial + coefficients[i] + "x^" + i;
@@ -61,7 +74,10 @@ public class Polynomial {
                 else if (coefficients[i] < 0) {
                     polynomial = polynomial + " - " + (-coefficients[i]) + "x^" + i;
                 }
+                // Specifically omitting the case when the coefficient is 0, because it should
+                // not be printed.
             }
+            // Not the first coefficient printed, print as usual
             else {
                 if (coefficients[i] > 0) {
                     polynomial = polynomial + " + " + coefficients[i] + "x^" + i;
