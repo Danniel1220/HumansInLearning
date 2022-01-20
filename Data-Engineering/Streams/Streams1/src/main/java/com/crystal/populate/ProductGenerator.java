eb55cd828b22800c87c8f8c13f8d68cddf83a023
george.clim <climdanniel@gmail.com>
2022-01-20 13:24:13 +0200

    Data-Engineering/Streams/Streams 1
    Added functionality to ProductGenerator to prevent duplicate ids when generating products.

--- Data-Engineering/Streams/Streams1/src/main/java/com/crystal/populate/ProductGenerator.java
+++ Data-Engineering/Streams/Streams1/src/main/java/com/crystal/populate/ProductGenerator.java
@@ -9,34 +9,30 @@ import java.util.Random;
 public class ProductGenerator {
     public List<Product> generateProducts () {
         List<Product> productList = new ArrayList<>();
+        List<Integer> alreadyGeneratedIds = new ArrayList<>();
 
+        generateProductCategory(productList, alreadyGeneratedIds, "Book", "Books");
+        generateProductCategory(productList, alreadyGeneratedIds, "Baby Product", "Baby");
+        generateProductCategory(productList, alreadyGeneratedIds, "Toy", "Toys");
+
+        return productList;
+    }
+
+    private void generateProductCategory(List<Product> productList, List<Integer> alreadyGeneratedIds,
+                          String name, String category) {
         Random random = new Random();
 
         for (int i = 0; i < 100; i++) {
+            int id = random.nextInt(99999 - 10000) + 10000;
+            while (alreadyGeneratedIds.contains(id)) {
+                id = random.nextInt(99999 - 10000) + 10000;
+            }
             productList.add(new Product(
-                    random.nextInt(99999 - 10000) + 10000,
-                    "Book " + i,
-                    "Books",
+                    id,
+                    name + i,
+                    category,
                     random.nextInt(999 - 100) + 100));
+            alreadyGeneratedIds.add(id);
         }
-
-        for (int i = 0; i < 100; i++) {
-            productList.add(new Product(
-                    random.nextInt(99999 - 10000) + 10000,
-                    "Baby Product " + i,
-                    "Baby",
-                    random.nextInt(999 - 100) + 100));
-        }
-
-
-        for (int i = 0; i < 100; i++) {
-            productList.add(new Product(
-                    random.nextInt(99999 - 10000) + 10000,
-                    "Toy " + i,
-                    "Toys",
-                    random.nextInt(999 - 100) + 100));
-        }
-
-        return productList;
     }
 }
