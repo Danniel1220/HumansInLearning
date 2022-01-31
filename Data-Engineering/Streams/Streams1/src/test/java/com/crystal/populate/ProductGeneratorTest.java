a820367309d557e632ef41a51816b2636d422b85
george.clim <climdanniel@gmail.com>
2022-01-31 10:55:24 +0200

    Data-Engineering/Streams/Streams 1
    unit testing

---
+++ Data-Engineering/Streams/Streams1/src/test/java/com/crystal/populate/ProductGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.crystal.populate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductGeneratorTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+}
