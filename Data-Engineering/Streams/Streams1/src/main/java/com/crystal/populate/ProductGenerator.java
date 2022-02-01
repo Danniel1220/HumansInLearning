65ee04145b22c1aa20d527773c0ee76e837f4912
george.clim <climdanniel@gmail.com>
2022-02-01 14:07:02 +0200

    Data-Engineering/Streams/Streams 1
    Refactored ProductGenerator for unit testing.
    Wrote tests for ProductGenerator.

--- Data-Engineering/Streams/Streams1/src/main/java/com/crystal/populate/ProductGenerator.java
+++ Data-Engineering/Streams/Streams1/src/main/java/com/crystal/populate/ProductGenerator.java
@@ -10,25 +10,27 @@ public class ProductGenerator {
     public List<Product> generateProducts () {
         List<Product> productList = new ArrayList<>();
         List<Integer> alreadyGeneratedIds = new ArrayList<>();
+        Random random = new Random();
 
-        generateProductCategory(productList, alreadyGeneratedIds, "Book", "Books");
-        generateProductCategory(productList, alreadyGeneratedIds, "Baby Product", "Baby");
-        generateProductCategory(productList, alreadyGeneratedIds, "Toy", "Toys");
+        productList.addAll(generateProductCategory(alreadyGeneratedIds, "Book", "Books", 100, random));
+        productList.addAll(generateProductCategory(alreadyGeneratedIds, "Baby Product", "Baby", 100, random));
+        productList.addAll(generateProductCategory(alreadyGeneratedIds, "Toy", "Toys", 100, random));
 
         return productList;
     }
 
-    // make this return a list
-    private void generateProductCategory(List<Product> productList, List<Integer> alreadyGeneratedIds,
-                          String name, String category) {
-        Random random = new Random();
+    public List<Product> generateProductCategory(List<Integer> alreadyGeneratedIds, String name, String category,
+                                                 int productAmount, Random random) {
+        List<Product> products = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < productAmount; i++) {
+            System.out.println("Generating product " + i);
+
             int id = random.nextInt(99999 - 10000) + 10000;
             while (alreadyGeneratedIds.contains(id)) {
                 id = random.nextInt(99999 - 10000) + 10000;
             }
-            productList.add(new Product(
+            products.add(new Product(
                     id,
                     name + i,
                     category,
@@ -36,5 +38,7 @@ public class ProductGenerator {
 
             alreadyGeneratedIds.add(id);
         }
+
+        return products;
     }
 }
--- Data-Engineering/Streams/Streams1/src/test/java/com/crystal/populate/ProductGeneratorTest.java
+++ Data-Engineering/Streams/Streams1/src/test/java/com/crystal/populate/ProductGeneratorTest.java
@@ -1,12 +1,54 @@
 package com.crystal.populate;
 
+import com.crystal.dao.Product;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class ProductGeneratorTest {
+    ProductGenerator productGenerator;
+
+    @Before
+    public void init() {
+        productGenerator = new ProductGenerator();
+    }
 
     @Test
-    public void dummyTest() {
-        Assert.assertTrue(true);
+    public void shouldGenerate5DigitIds() {
+        // Arrange (creating class instances for instance)
+        List<Product> productList = productGenerator.generateProducts();
+        // Act (creating the actual data, that we need to test)
+        // Assert (do asserts)
+        productList.forEach(p -> Assert.assertTrue("Product had invalid id: " + p.getId(),
+                p.getId() >= 10000 && p.getId() <= 99999)
+        );
     }
+
+    @Test
+    public void shouldGenerate3DigitPrices() {
+        // Arrange (creating class instances for instance)
+        List<Product> productList = productGenerator.generateProducts();
+        // Act (creating the actual data, that we need to test)
+        // Assert (do asserts)
+        productList.forEach(p -> Assert.assertTrue("Product had invalid price: " + p.getPrice(),
+                p.getPrice() >= 100 && p.getPrice() <= 999)
+        );
+    }
+
+    @Test
+    public void shouldNotHaveDuplicateIds() {
+        Random random = new Random();
+        List<Product> productList = productGenerator.generateProductCategory(new ArrayList<>(), "Toy",
+                "Toys", 1000, random);
+        List<Long> productIdsList = productList.stream().mapToLong(Product::getId).boxed().collect(Collectors.toList());
+
+        Set<Long> productIdsSet = new HashSet<>(productIdsList);
+
+        Assert.assertFalse("Found duplicate ids", productIdsSet.size() < productIdsList.size());
+    }
+
+
 }
