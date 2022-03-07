b27ad1d3025c2f68a3abd08eebb983e19f568a68
Clim Daniel George <climdanniel@gmail.com>
2022-03-07 17:38:21 +0200

    Created NearEarthObjects project.
    NearEarthObjectAPIManager connects via an http url connection to NASA's NEO api using the demo key and prints results to screen.

---
+++ Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/api/NearEarthObjectAPIManager.java
@@ -0,0 +1,50 @@
+package com.crystal.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class NearEarthObjectAPIManager {
+    private final String NEAR_EARTH_OBJECT_DEMO_KEY = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY";
+    HttpURLConnection httpConnection;
+
+    public NearEarthObjectAPIManager() {
+        StringBuffer responseContent = new StringBuffer();
+
+        try {
+            URL url = new URL(NEAR_EARTH_OBJECT_DEMO_KEY);
+            httpConnection = (HttpURLConnection)url.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setConnectTimeout(5000);
+            httpConnection.setReadTimeout(5000);
+            int status = httpConnection.getResponseCode();
+            System.out.println("Connection status: " + status);
+            String line;
+            BufferedReader reader;
+
+            if (status == 200) {
+                reader = new BufferedReader(new InputStreamReader(this.httpConnection.getInputStream()));
+            } else {
+                reader = new BufferedReader(new InputStreamReader(this.httpConnection.getErrorStream()));
+            }
+
+            while((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+
+            reader.close();
+            System.out.println(responseContent);
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/run/Run.java
@@ -0,0 +1,10 @@
+package com.crystal.run;
+
+import com.crystal.api.NearEarthObjectAPIManager;
+
+public class Run {
+
+    public static void main(String[] args) {
+        NearEarthObjectAPIManager nearEarthObjectAPIManager = new NearEarthObjectAPIManager();
+    }
+}
