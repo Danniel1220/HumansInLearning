8953ff142ba386b799d78fc2d7d6ef38cf57f63e
Clim Daniel George <climdanniel@gmail.com>
2022-03-15 18:19:33 +0200

    Updated the NEO DTO
    closeApproachDate is now a string. Temporary fix for easier implementation of the json parsinf.

--- Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/dto/NearEarthObject.java
+++ Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/dto/NearEarthObject.java
@@ -2,18 +2,20 @@ package com.crystal.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.Date;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class NearEarthObject {
     private final long id;
     private final String name;
     private final float estimatedDiameterMin; // in meters
     private final float estimatedDiameterMax; // in meters
     private final boolean isPotentiallyHazardous;
-    private final Date closeApproachDate;
+    private final String closeApproachDate;
     private final float relativeVelocity; // in kilometers/second
     private final float missDistance; // in kilometers
     private final String orbitingBody;
