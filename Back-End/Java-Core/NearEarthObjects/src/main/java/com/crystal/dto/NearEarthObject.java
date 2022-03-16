2bf284a5dfe6224bf1eda29b74eb359e60c9693b
Clim Daniel George <climdanniel@gmail.com>
2022-03-16 11:21:53 +0200

    Updated neo DTO
    Miss distance is now stored in AU

--- Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/dto/NearEarthObject.java
+++ Back-End/Java-Core/NearEarthObjects/src/main/java/com/crystal/dto/NearEarthObject.java
@@ -1,6 +1,7 @@
 package com.crystal.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,6 +10,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Getter
 @ToString
+@EqualsAndHashCode
 public class NearEarthObject {
     private final long id;
     private final String name;
@@ -17,7 +19,8 @@ public class NearEarthObject {
     private final boolean isPotentiallyHazardous;
     private final String closeApproachDate;
     private final float relativeVelocity; // in kilometers/second
-    private final float missDistance; // in kilometers
+    private final float missDistance; // in astronomical units (unit of length effectively equal to the average,
+                                      // or mean, distance between Earth and the Sun, defined as 149,597,870.7 km)
     private final String orbitingBody;
     private final boolean isSentryObject;
 }
