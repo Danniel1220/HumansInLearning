a99c7cc9252737ae5dfe5dbd5d33e1aa9f1998d4
Clim Daniel George <climdanniel@gmail.com>
2022-03-29 02:21:03 +0300

    Update ParkingZonesManager.java
    Added method for displaying all parking zones.
    addVehiclesArrayToParkZone now handles ZoneNotFoundException
    Added method addVehicleToParkZone
    Added method getIndexOfParkTreeZoneByName

--- Data-Engineering/ListToParkingZoneTreeStructure/src/main/java/com/crystal/parking/ParkingZonesManager.java
+++ Data-Engineering/ListToParkingZoneTreeStructure/src/main/java/com/crystal/parking/ParkingZonesManager.java
@@ -1,5 +1,7 @@
 package com.crystal.parking;
 
+import com.crystal.entity.Vehicle;
+import com.crystal.exceptions.ZoneNotFoundException;
 import com.crystal.stat.ParkStatus;
 import com.crystal.tree.ParkTree;
 import lombok.AllArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.Getter;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -15,26 +18,69 @@ public class ParkingZonesManager {
     private List<ParkTree> parkingZones;
 
     public void addParkZone (ParkTree parkTree) {
+        parkingZones.add(parkTree);
+    }
 
+    public void displayParkZones() {
+        StringBuilder parkZonesString = new StringBuilder();
+        for (ParkTree p: parkingZones) {
+            parkZonesString.append(p.toString() + "\n\n");
+        }
+        System.out.println(parkZonesString.toString());
     }
 
     public void addVehiclesArrayToParkZone(JSONArray vehiclesArray) {
         for (int i = 0; i < vehiclesArray.length(); i++) {
-            JSONObject vehicleJsonObject = new JSONObject(vehiclesArray.getJSONObject(i));
+            JSONObject vehicleJsonObject = vehiclesArray.getJSONObject(i);
 
             if (ParkStatus.valueOf(vehicleJsonObject.getString("status")).equals(ParkStatus.OK)) {
-                System.out.println("Status OK");
+                try {
+                    addVehicleToParkZone(vehicleJsonObject);
+                } catch (ZoneNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
             else {
                 System.out.println("Status error");
             }
-
-            addVehicleToParkZone(vehicleJsonObject);
         }
     }
 
-    public void addVehicleToParkZone(JSONObject vehicleJsonObject) {
+    public void addVehicleToParkZone(JSONObject vehicleJsonObject) throws ZoneNotFoundException {
+        JSONArray vehicleZonePathArray = vehicleJsonObject.getJSONArray("zone");
 
+        List<String> zonePath = new ArrayList<String>();
+        for (int i = 0; i < vehicleZonePathArray.length(); i++) {
+            zonePath.add(vehicleZonePathArray.getJSONObject(i).getString("zoneId"));
+        }
 
+        // Fetching the level 1 zone of the vehicle
+        ParkTree zoneToAddVehicleTo = parkingZones.get(getIndexOfParkTreeZoneByName(parkingZones, zonePath.get(0)));
+
+        // If the zone we are looking for isn't a level 1 zone
+        if (zonePath.size() > 1) {
+            for (int i = 1; i < zonePath.size(); i++) {
+                int zoneIndex = getIndexOfParkTreeZoneByName(zoneToAddVehicleTo.getSubZones(), zonePath.get(i));
+                if (zoneIndex >= 0) {
+                    zoneToAddVehicleTo = zoneToAddVehicleTo.getSubZones().get(zoneIndex);
+                }
+                else {
+                    throw new ZoneNotFoundException("Zone " + zonePath.get(i) + " could not be found...");
+                }
+
+            }
+        }
+
+        // Adding the vehicle to the zone we fetched by traversing the zonePath
+        zoneToAddVehicleTo.getVehicles().add(Vehicle.getVehicleObjectFromJsonObject(vehicleJsonObject));
+    }
+
+    public int getIndexOfParkTreeZoneByName(List<ParkTree> parkingZones, String parkingZoneName) {
+        for(int i = 0; i < parkingZones.size(); i++) {
+            if (parkingZones.get(i).getZoneName().equals(parkingZoneName)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
